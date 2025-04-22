509639c62e991a4966deea44f58f4ef888812973
virginiapaolini <virginia.paolini@edu.unifi.it>
2025-04-22 13:01:08 +0200

    aggiunto codice versione programma

---
+++ versioj.h
@@ -0,0 +1,12 @@
+//
+// Created by Virginia Paolini on 22/04/25.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSIOJ_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSIOJ_H
+
+#include <string>
+
+static const std::string VERSION = "v1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSIOJ_H
